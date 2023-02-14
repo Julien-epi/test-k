6a3fd0c68af05e0b1bdafe9aab044fe1de68b97e
Julien-epi <julienranson77@gmail.com>
2023-02-14 19:45:12 +0100

    adding modal loading

--- src/pages/ListTraining.tsx
+++ src/pages/ListTraining.tsx
@@ -8,11 +8,12 @@ const ninja = require("../img/ninja.png");
 export default function ListTraining() {
   const [training, setTraining] = useState<TrainingData[]>([]);
   const [loadingButton, setLoadingButton] = useState<boolean[]>([]);
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     setTraining(dataTraining);
     relistTable();
-  }, [training]);
+  }, []);
 
   function relistTable() {
     dataTraining.sort((a, b) => {
@@ -23,7 +24,7 @@ export default function ListTraining() {
   const handleClick = (index: number) => {
     const newLoadingArray = [...loadingButton];
     const tempArray = [...training];
-
+    setShowModal(true);
     newLoadingArray[index] = true;
     tempArray[index].status = Math.random() > 0.5 ? "Victoire" : "Défaite";
     setLoadingButton(newLoadingArray);
@@ -32,10 +33,11 @@ export default function ListTraining() {
       updatedLoadingArray[index] = false;
 
       setLoadingButton(updatedLoadingArray);
+      setShowModal(false);
     }, 1000);
+
     relistTable();
   };
-
 
   return (
     <div className="flex justify-evenly mt-5">
@@ -43,7 +45,7 @@ export default function ListTraining() {
         <img className="w-25 h-40" src={ninja} alt="ninja_picture" />
       </div>
       <div>
-        <table>
+        <table className="table-fixed" >
           <caption>List Training</caption>
           <thead>
             <tr>
@@ -58,26 +60,18 @@ export default function ListTraining() {
               return (
                 <tbody key={index}>
                   <tr>
-                    <td className="p-2 border">{item.title}</td>
-                    <td className="p-2 border">{item.category}</td>
-                    <td className="p-2 border">{item.status}</td>
-                    <td className="p-2 border">
+                    <td className="h-16 p-2 border">{item.title}</td>
+                    <td className="h-16 p-2 border">{item.category}</td>
+                    <td className="h-16 p-2 border">{item.status}</td>
+                    <td className="h-16 p-2 border">
                       {item.status === "A commencer" ||
                       item.status === "Défaite" ? (
-                        loadingButton[index] ? (
-                          <HashLoader
-                            className="m-2 p-2 rounded-xl bg-red-600 hover:bg-red-700 text-white font-bold"
-                            color="white"
-                            size={30}
-                          />
-                        ) : (
-                          <button
-                            className="m-2 p-2 rounded-xl bg-red-600 hover:bg-red-700 text-white font-bold"
-                            onClick={() => handleClick(index)}
-                          >
-                            GO
-                          </button>
-                        )
+                        <button
+                          className="m-2 p-2 rounded-xl bg-red-600 hover:bg-red-700 text-white font-bold"
+                          onClick={() => handleClick(index)}
+                        >
+                          GO
+                        </button>
                       ) : (
                         ""
                       )}
@@ -92,6 +86,16 @@ export default function ListTraining() {
       <div>
         <img className="w-25 h-40" src={ninja} alt="ninja_picture" />
       </div>
+
+      {/* Modal */}
+
+      {showModal && (
+        <div className="fixed top-[40%] flex justify-center">
+          <div className="bg-black/[.3] p-20 rounded-3xl">
+            <HashLoader color="black" size={50} />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
